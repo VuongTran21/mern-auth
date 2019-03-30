e5e290601b02c93fc26ae8b8121f824a4268fd74
hoangvuong.tran21 <hoangvuong.tran21@gmail.com>
2019-03-30 13:07:45 +0700

    Add client, login and register form

---
+++ client/src/App.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import { Route } from 'react-router-dom';
+import './App.css';
+import Navbar from './components/layout/Navbar';
+import Landing from './components/layout/Landing';
+import Register from './components/auth/Register';
+import Login from './components/auth/Login';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Navbar />
+        <Route exact path="/" component={Landing} />
+        <Route exact path="/register" component={Register} />
+        <Route exact path="/login" component={Login} />
+      </div>
+    );
+  }
+}
+
+export default App;
