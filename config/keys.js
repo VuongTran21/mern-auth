6fc7c75e5c2bae0466e05c54578bc608d2cdfd07
hoangvuong.tran21 <hoangvuong.tran21@gmail.com>
2019-04-03 15:21:56 +0700

    Add Change

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,7 @@
+// mongodb://hoangvuong:123123XXX@ds125526.mlab.com:25526/hoangvuong-db
+// mongodb://<dbuser>:<dbpassword>@ds113505.mlab.com:13505/mern-auth-demo
+// mongodb://127.0.0.1:27017/mern-auth
 module.exports = {
-    mongoURI: "mongodb://127.0.0.1:27017/mern-auth",
+    mongoURI: "mongodb://hoangvuong:123123XXX@ds113505.mlab.com:13505/mern-auth-demo",
     secretOrKey: "secret" 
   };
